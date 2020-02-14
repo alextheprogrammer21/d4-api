7e007d6f75c93132bcd19eda873d4f4a4f86f6a6
Alex <alextheprogrammer21@gmail.com>
2020-02-14 00:00:07 +0000

    c

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,20 @@
 const request = require('request');
-let breedName = process.argv.slice(2);
-let URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
 
-request(URL, (error, response, body) => {
-  const data = JSON.parse(body); let dataArray = Object.keys(data); let data2 = data[dataArray];
 
-  if (!data2) return console.error('error: invalid type of breed');
-  
-  if (error) return console.error(`Error: ${error}`);
-  
-  
-  console.log(data2.description);
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  let URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+
+  request(URL, (error, response, body) => {
+    const data = JSON.parse(body); let dataArray = Object.keys(data); let data2 = data[dataArray];
+
+    if (!data2) return callback(error, null);
+    
+    // if (error) return console.error(`Error: ${error}`);
+    
+    callback(null, data2.description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
